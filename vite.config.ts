553b17619d118d3458b09f1823ac4160f74ab0bd
Ahsaan <ahsaanturk@gmail.com>
2026-02-16 13:33:24 +0500

    Offline Styling (not necessery)

--- vite.config.ts
+++ vite.config.ts
@@ -36,6 +36,9 @@ export default defineConfig(({ mode }) => {
               type: 'image/png'
             }
           ]
+        },
+        workbox: {
+          globPatterns: ['**/*.{js,css,html,ico,png,svg,woff,woff2,ttf}']
         }
       })
     ],
